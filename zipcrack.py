daafe53d7afbecd3a2266ee5f2d3b5b161a38074
3x1t1um <54857145+3x1t1um@users.noreply.github.com>
2019-12-21 15:32:28 +0100

    Update zipcrack.py

--- zipcrack.py
+++ zipcrack.py
@@ -1,8 +1,10 @@
+# importation of modules
 from zipfile import ZipFile
 import sys
 import argparse
 import os
 
+# recover the requested files
 def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('passlist',type=str, help='Set your password list (*.txt)')
@@ -10,6 +12,7 @@ def main():
 	args = parser.parse_args()
 	check(args.passlist, args.zipfile)
 
+# check if the given files are valid
 def check(passlist, zipfile):
 	if os.path.isfile(passlist) & os.path.isfile(zipfile):
 		bf(passlist, zipfile)
@@ -17,21 +20,22 @@ def check(passlist, zipfile):
 		print('[ * ] File Error')
 		sys.exit(1)
 
+# start brute force
 def bf(passlist, zipfile):
-	count = 1
+	count = 1 # counter
 	with open(passlist,'r') as liste:
-		for passwd in liste.readlines():
+		for passwd in liste.readlines(): # take all passwords 1 by 1
 			try:
-				zFile = ZipFile(zipfile)
-				zFile.extractall(pwd=passwd.replace('\n', '').encode('utf-8'))
+				zfile = ZipFile(zipfile)
+				zfile.extractall(pwd=passwd.replace('\n', '').encode('utf-8')) # extracting files in the zip
 				print('\n#=========================================#')
 				print('	[ %s ] Password found : %s ' % (count, passwd.replace('\n', '')))
 				print('#=========================================#')
-				break
+				break # exit
 			except:
 				print('[ %s ] Password failed : %s ' % (count, passwd.replace('\n', '')))
-				count += 1
+				count += 1 # update the counter
 
-
+# run the script
 if __name__ == '__main__':
 	main()
